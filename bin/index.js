c13cd1564c63e8e6682b3da780a1b7cbd3d3c083
limengke <752140235@qq.com>
2019-06-21 16:56:58 +0800

    feat: 初始化项目

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#! usr/bin/env node
+
+const mock = require('../lib/index')
+
+const args = process.argv.slice(2)
+
+mock(args)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const mock = function () {
+
+}
+
+export default mock
