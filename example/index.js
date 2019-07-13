bb92ae6274f8925808937e88baae48bc56ca5998
limengke <752140235@qq.com>
2019-07-13 16:50:42 +0800

    docs: 更新 example

--- example/index.js
+++ example/index.js
@@ -1,13 +1,4 @@
-const path = require('path')
 const { mock } = require('../lib/index.js')
 
-const swaggerPath = path.resolve(__dirname, './.swagger')
-const schemaPath = path.resolve(__dirname, '.')
-
-mock({
-    schemaOption: {
-        schemaPath,
-        swaggerPath
-    }
-})
+mock()
 
